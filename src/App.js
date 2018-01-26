44f11de25ef9f28f7ea4cb11f5185dae8789d9e1
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2018-01-26 14:05:30 +0100

    seperate component

--- src/App.js
+++ src/App.js
@@ -12,10 +12,11 @@ import * as Colors from 'material-ui/styles/colors';
 import IconButton from 'material-ui/IconButton';
 import ImageFlashOn from 'material-ui/svg-icons/image/flash-on';
 import Chip from 'material-ui/Chip';
-import {Bar} from 'react-chartjs-2';
+//import {Bar} from 'react-chartjs-2';
 import RaisedButton from 'material-ui/RaisedButton';
 
 import axios from 'axios';
+import Chart from './chart.js'
 
 const styles = {
     chip: {
@@ -39,82 +40,14 @@ const titleStyles = {
   }
 };
 
-const data = {
-  labels: [0,1,2,3,4,5,6,7,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-  datasets: [
-    {
-      label: 'Consumption',
-      backgroundColor: '#0277bd',
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 55],
-      stack: 1
-    },
-    {
-      label: 'Electric Vehichle',
-      backgroundColor: '#58a5f0',
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 55],
-      stack: 1
-    },
-    {
-      label: 'Photovaltaics',
-      type: 'bar',
-      backgroundColor: 'rgba(85,139,47,0.5)',
-      borderColor: 'rgba(133,187,92,0.6)',
-      borderWidth: 2,
-      hoverBackgroundColor: 'rgba(133,187,92,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 55],
-      stack: 2
-    }
-  ]
 
-}
 
 class App extends Component {
   
-  constructor() {
-    super();
-    this.state = {
-      date: [],
-      fetched: false
-    };
-  }
-  componentDidMount() {
-    axios.get('https://json-ibaoiethzi.now.sh/testok.json')
-    //https://https://json-ibaoiethzi.now.sh/consumption.json
-    .then(res => {
-      const data = res.data;
-      console.log(data)
-      this.setState ({
-        date: data.Date,
-        fetched: true
-      })
-    })
-  }
-
-  fetching() {
-    if (this.state.fetched) return <p>(Fetched)</p>;
-    return <p>(Not Fetched)</p>;
-  }
 
   render() {
 //want to iterate the date from api 
-  const dataDate = this.state.date 
-   const DateList = function(props){
-     console.log(props.dateDate);
-     return (
-      <ul>
-        {
-          props.dataDate.map(function(item){
-            return <li> {item} </li>
-          })
-        }
-      </ul>
-     )
-   }
+
     
     const rightButtons = (
       <div>
@@ -129,6 +62,9 @@ class App extends Component {
         <IconButton style={{color:'Colors.grey200'}}><ImageFlashOn /></IconButton>
         </div>
     );
+
+ //return HTML
+
     return (
       <div>
      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)} >
@@ -151,47 +87,14 @@ class App extends Component {
       </div>
       <br/>
       <br/>
-      <div className = "flex-container">
-      <Bar
-          data={data}
-          options={{
-            maintainAspectRatio: false,
-            legend: {
-              labels: {
-                  // This more specific font property overrides the global property
-                  fontColor: '#bdbdbd'
-              }
-            },
-            scales:{
-              xAxes: [{
-              stacked: true,
-              ticks: {
-                fontColor: '#bdbdbd',
-                fontSize: 12,
-              },
-              gridLines: {
-                color: "rgba(0, 0, 0, 0)",
-                
-              
-              }
-              }],
-              yAxes: [{
-              stacked: true,
-              ticks: {
-                fontColor: '#bdbdbd',
-                fontSize: 12,
-              },
-              }]
-          }
-          }}
-        />
-      </div>
+      
+      <Chart />
+    
       </div>
       </Card>
       <br />
-      <RaisedButton label="Update" primary={true}  />
-       {this.fetching()}
-     
+      <RaisedButton label="Update" primary={true}  />  
+
       </MuiThemeProvider>
       
          
